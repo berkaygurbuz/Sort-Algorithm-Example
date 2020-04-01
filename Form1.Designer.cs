e61021aeb7e2520a8fcc1b1b1bc5b5524110e341
berkaygurbuz <berkay-gurbuz@hotmail.com>
2020-04-01 23:35:28 +0300

    update sortalgorithm

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SortAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bubble Sort",
+            "Selection Sort",
+            "Insertion Sort"});
+            this.comboBox1.Location = new System.Drawing.Point(33, 230);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(174, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Select Sort Algorithm";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(33, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter the numbers";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(258, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(130, 292);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(254, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Entered Numbers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(406, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Big to small";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(410, 75);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(138, 292);
+            this.listBox2.TabIndex = 5;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 18;
+            this.listBox3.Location = new System.Drawing.Point(572, 75);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(138, 292);
+            this.listBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(568, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Small to big";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 508);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        int[] numbers = new int[10];
+        
+        //check if input is an integer or a string
+        static bool isNumber(string s)
+        {
+            int distance;
+            while (!int.TryParse(s,out distance))
+            {
+                return false;
+            }
+            return true;
+        }
+        
+
+        void bubbleSort_smalltobig(int[] Array)
+        {
+            int i;
+            int j;
+            int TempValue;
+
+            for (i = (Array.Length - 1); i >= 0; i--)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    if (Array[j - 1] > Array[j])
+                    {
+                        TempValue = Array[j - 1];
+                        Array[j - 1] = Array[j];
+                        Array[j] = TempValue;
+                    }
+                }
+            }
+        }
+
+        void bubbleSort_bigtosmall(int[] Array)
+        {
+            int i;
+            int j;
+            int TempValue;
+
+            for (i = (Array.Length - 1); i >= 0; i--)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    if (Array[j - 1] < Array[j])
+                    {
+                        TempValue = Array[j - 1];
+                        Array[j - 1] = Array[j];
+                        Array[j] = TempValue;
+                    }
+                }
+            }
+        }
+
+        void selectionSort_smalltobig(int[] Array)
+        {
+            int n = Array.Length;
+
+            // One by one move boundary of unsorted subarray 
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Find the minimum element in unsorted array 
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (Array[j] < Array[min_idx])
+                        min_idx = j;
+
+                // Swap the found minimum element with the first 
+                // element 
+                int temp = Array[min_idx];
+                Array[min_idx] = Array[i];
+                Array[i] = temp;
+            }
+        }
+
+        void selectionSort_bigtosmall(int[] Array)
+        {
+            int n = Array.Length;
+
+            // One by one move boundary of unsorted subarray 
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Find the minimum element in unsorted array 
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (Array[j] > Array[min_idx])
+                        min_idx = j;
+
+                // Swap the found minimum element with the first 
+                // element 
+                int temp = Array[min_idx];
+                Array[min_idx] = Array[i];
+                Array[i] = temp;
+            }
+        }
+        void insertionSort_smalltobig(int[] Array)
+        {
+            int i;
+            int j;
+            int IndexValue;
+
+            for (i = 1; i < Array.Length; i++)
+            {
+                IndexValue = Array[i];
+                j = i;
+
+                while ((j > 0) && (Array[j - 1] > IndexValue))
+                {
+                    Array[j] = Array[j - 1];
+                    j = j - 1;
+                }
+
+                Array[j] = IndexValue;
+            }
+        }
+
+        void insertionSort_bigtosmall(int[] Array)
+        {
+            int i;
+            int j;
+            int IndexValue;
+
+            for (i = 1; i < Array.Length; i++)
+            {
+                IndexValue = Array[i];
+                j = i;
+
+                while ((j > 0) && (Array[j - 1] < IndexValue))
+                {
+                    Array[j] = Array[j - 1];
+                    j = j - 1;
+                }
+
+                Array[j] = IndexValue;
+            }
+        }
+
+
+
+
+    public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //If the bubble sort is selected
+            if (comboBox1.SelectedIndex == 0)
+            {
+                bubbleSort_smalltobig(numbers);
+                listBox3.Items.Clear();
+                //enter the numbers listbox3
+                for (int i = 0; i < 10; i++)
+                {
+                    
+                    listBox3.Items.Add(numbers[i]);
+                }
+
+                bubbleSort_bigtosmall(numbers);
+                listBox2.Items.Clear();
+                //enter the numbers listbox2
+                for (int i = 0; i < 10; i++)
+                {
+                    
+                    listBox2.Items.Add(numbers[i]);
+                }
+
+            }//If the selection sort is selected
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                selectionSort_smalltobig(numbers);
+                listBox3.Items.Clear();
+                //enter the number listbox3
+                for (int i = 0; i < 10; i++)
+                {
+                    
+                    listBox3.Items.Add(numbers[i]);
+                }
+
+                selectionSort_bigtosmall(numbers);
+                //enter the numbers listbox2
+                listBox2.Items.Clear();
+                for (int i = 0; i < 10; i++)
+                {
+                    listBox2.Items.Add(numbers[i]);
+                }
+            }//if the insertion sort is selected
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                insertionSort_smalltobig(numbers);
+                listBox3.Items.Clear();
+                //enter the numbers listbox3
+                for(int i = 0; i < 10; i++)
+                {
+                    listBox3.Items.Add(numbers[i]);
+                }
+                insertionSort_bigtosmall(numbers);
+                listBox2.Items.Clear();
+                //enter the numbers listbox2
+                for(int i = 0; i < 10; i++)
+                {
+                    listBox2.Items.Add(numbers[i]);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            //Getting number from user
+            for(int i = 0; i < 10; i++)
+            {
+                string enteredNumber = Interaction.InputBox("Enter the"+ i+". Number", "Getting Number", "Exp: 25 ", 0, 0);
+                if (isNumber(enteredNumber))    //Check the entered number is integer or string
+                {
+                    numbers[i] = Convert.ToInt32(enteredNumber);
+                }
+                else
+                {
+                    //if input is string
+                    i--;
+                }
+            }
+
+            //Write the numbers to listbox1
+            for(int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add(numbers[i]);
+            }
+
+            
+        }
+    }
+}
